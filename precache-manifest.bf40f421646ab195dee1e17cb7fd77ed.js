fe6a40c1e8fd3058806c02154d3eff6151b2fcd7
Kevin Tran <kevin.tran.kt@gmail.com>
2019-12-30 17:08:08 -0800

    Initial commit

---
+++ precache-manifest.bf40f421646ab195dee1e17cb7fd77ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9857b991f58919124b14356ba8b04895",
+    "url": "/index.html"
+  },
+  {
+    "revision": "861c4be514ef8531e89f",
+    "url": "/static/css/2.aa46e993.chunk.css"
+  },
+  {
+    "revision": "22b79fe926e9505b2efc",
+    "url": "/static/css/main.1882af2b.chunk.css"
+  },
+  {
+    "revision": "861c4be514ef8531e89f",
+    "url": "/static/js/2.d5929d65.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d5929d65.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22b79fe926e9505b2efc",
+    "url": "/static/js/main.dbabda83.chunk.js"
+  },
+  {
+    "revision": "b0d905d2d1ba2e0d6578",
+    "url": "/static/js/runtime-main.b411a41a.js"
+  }
+]);
